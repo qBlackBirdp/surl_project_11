88f16ef51ef7b2daf717c8b1285be7a25af9ba6b
qBlackbirdp <blackbird9711@gmail.com>
2024-11-05 14:21:07 +0900

    URL 단축 방식 설정

--- src/main/java/com/hys/exam/surl_project_11/SurlController.java
+++ src/main/java/com/hys/exam/surl_project_11/SurlController.java
@@ -1,9 +1,12 @@
 package com.hys.exam.surl_project_11;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -20,6 +23,29 @@ public class SurlController {
                 .body(body)
                 .url(url)
                 .build();
+
+        surls.add(surl);
         return surl;
+    }
+
+    @GetMapping("/s/{body}/**")
+    @ResponseBody
+    public String add(
+            @PathVariable String body,
+            HttpServletRequest req
+    ) {
+        String url = req.getRequestURI();
+
+        if (req.getQueryString() != null) {
+            url += "?" + req.getQueryString();
+        }
+
+        String[] urlBits = url.split("/", 4);
+
+        System.out.println("Arrays.toString(urlBits) : " + Arrays.toString(urlBits));
+
+        url = urlBits[3];
+
+        return url;
     }
 }
