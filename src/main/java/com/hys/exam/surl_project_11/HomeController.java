8bccd3331aa8ed46268c997daf621a958b579bdf
qBlackbirdp <blackbird9711@gmail.com>
2024-11-07 10:55:14 +0900

    자격증명

--- src/main/java/com/hys/exam/surl_project_11/HomeController.java
+++ src/main/java/com/hys/exam/surl_project_11/HomeController.java
@@ -10,9 +10,18 @@ public class HomeController {
     @Value("${custom.site.name}")
     private String customSiteName;
 
+    @Value("${custom.secret.key}")
+    private String secretKey;
+
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
         return "Main on" + customSiteName;
+    }
+
+    @GetMapping("/secretKey")
+    @ResponseBody
+    public String showSecretKey() {
+        return "Main on" + secretKey;
     }
 }
