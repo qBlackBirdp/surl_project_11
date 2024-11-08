def41d3191789390aeee58790a4c447e239e3752
qBlackbirdp <blackbird9711@gmail.com>
2024-11-08 11:43:51 +0900

    JPA 학습

--- src/main/java/com/hys/exam/surl_project_11/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/hys/exam/surl_project_11/domain/article/article/repository/ArticleRepository.java
@@ -3,6 +3,21 @@ package com.hys.exam.surl_project_11.domain.article.article.repository;
 import com.hys.exam.surl_project_11.domain.article.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    //    @Query("SELECT a FROM Article a WHERE a.id IN :ids ORDER BY a.title DESC, a.id ASC")
+    //    List<Article> findByIdsOrderByTitleDescIdAsc(@Param("ids") List<Long> ids);
+    List<Article> findByIdInOrderByTitleDescIdAsc(List<Long> ids);
+
+    //    @Query("SELECT a FROM Article a WHERE a.title LIKE %:keyword%")
+    //    List<Article> findByTitleLike(@Param("keyword") String keyword);
+    List<Article> findByTitleContaining(String keyword);
+
+
+    //    @Query("SELECT a FROM Article a WHERE a.title = :title AND a.body = :body")
+    //    List<Article> findByTitleAndBody(@Param("title") String title, @Param("body") String body);
+    List<Article> findByTitleAndBody(String title, String body);
 }
--- src/main/java/com/hys/exam/surl_project_11/global/initData/NotProd.java
+++ src/main/java/com/hys/exam/surl_project_11/global/initData/NotProd.java
@@ -3,40 +3,75 @@ package com.hys.exam.surl_project_11.global.initData;
 import com.hys.exam.surl_project_11.domain.article.article.entity.Article;
 import com.hys.exam.surl_project_11.domain.article.article.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Profile("!prod") // !prod == dev or test -> 운영 모드가 아님.
 @Configuration
 @RequiredArgsConstructor
 public class NotProd {
+    @Lazy
+    @Autowired
+    private NotProd self;
+    // this를 통한 객체 내부에서의 메서드 호출은 @Transactional을 작동시키지 않아
+    // 외부객에체 의한 메서드 호출은 @Transactional이 작동해
+    // @Lazy, @Autowired 조합은 this의 외부 호출 모드 버전 self를 얻을 수 있다.
+    // self를 통한 메서드 호출은 @Transactional을 작동 시킬 수 있어
+
     private final ArticleRepository articleRepository;
 
-    @Bean // 개발자가 new 하지 않아도 스프링이 직접 관리하는 객체
+    @Bean // 개발자가 new 하지 않아도 스프링부트가 직접 관리하는 객체
     public ApplicationRunner initNotProd() {
         return args -> {
-            System.out.println("Not Prod.initNotProd1");
-            System.out.println("Not Prod.initNotProd2");
-            System.out.println("Not Prod.initNotProd3");
-
-            Article articleFirst = Article.builder().
-                    title("제목1")
-                    .body("내용1").build();
-
-            Article articleSecond = Article.builder().
-                    title("제목2")
-                    .body("내용2").build();
-
-            System.out.println(articleFirst.getId());
-            System.out.println(articleSecond.getId());
-
-            articleRepository.save(articleFirst);
-            articleRepository.save(articleSecond);
-
-            System.out.println(articleFirst.getId());
-            System.out.println(articleSecond.getId());
+            self.work1();
+            self.work2();
         };
+    }
+
+    @Transactional
+    public void work1() {
+        if (articleRepository.count() > 0) return;
+
+        //articleRepository.deleteAll();
+
+        Article article1 = Article.builder().
+                title("제목1")
+                .body("내용1").build();
+
+        Article article2 = Article.builder().
+                title("제목2")
+                .body("내용2").build();
+
+
+        articleRepository.save(article1);
+        articleRepository.save(article2);
+
+        article2.setTitle("제목 2-2");
+
+        articleRepository.delete(article1);
+    }
+
+    @Transactional
+    public void work2() {
+        // List : 0 ~ N
+        // Optional : 0 ~ 1
+        Optional<Article> opArticle = articleRepository.findById(2L); // JpaRepository 기본 제공
+
+        List<Article> articles = articleRepository.findAll(); // JpaRepository 기본 제공
+
+        List<Article> articles1 = articleRepository.findByIdInOrderByTitleDescIdAsc(List.of(1L, 2L));
+
+        articleRepository.findByTitleContaining("제목");
+
+        articleRepository.findByTitleAndBody("제목", "내용");
+
     }
 }
